0a0783dbba71653d7182736a56236e0d0c909d2e
rtikha <raunak@guesthouser.com>
2020-05-20 01:46:46 +0530

    adding modal switch and css assembly

--- calculation.js
+++ calculation.js
@@ -21,59 +21,87 @@ var computedDates = [];
 var intellectualValues = [];
 var physicalValues = [];
 var emotionalValues = [];
+var fetchedDate;
 
-function dateDifference(from, to) {
-	from = new Date(from);
-	to = new Date(to);
+function getInput() {
 
-	const utc1 = Date.UTC(
-		from.getFullYear(),
-		from.getMonth(),
-		from.getDate(),
-	);
-	const utc2 = Date.UTC(
-		to.getFullYear(),
-		to.getMonth(),
-		to.getDate(),
-	);
-	const msPerDay = 1000 * 60 * 60 * 24;
-
-	return Math.floor((utc2 - utc1) / msPerDay);
+	fetchedDate = document.getElementById('dob').value;
+	fetchedDate = fetchedDate.split('/').reverse('').join('-');
+	fetchedDate = fetchedDate.replace(/\s+/g, '');
+	document.getElementById("first-hit").style.opacity="0";
+	document.getElementById("first-hit").style.transition = "opacity 0.3s"
+	getValues();
+	setTimeout(loadGraph,1000);
 }
 
-for (i = 1; i <= 30; i++) {
-	//past seven days
-	var back = new Date(today);
-	back.setDate(back.getDate() - i);
-	var ddBack = back.getDate();
-	var mmBack = back.getMonth() + 1;
-	var yyBack = back.getFullYear();
-	//backDates[i] = yyBack + '-' + mmBack + '-' + ddBack;
-
-	//future seven days
-	var future = new Date(today);
-	future.setDate(future.getDate() + i);
-	var ddFuture = future.getDate();
-	var mmFuture = future.getMonth() + 1;
-	var yyFuture = future.getFullYear();
-	futureDates[i] = yyFuture + '-' + mmFuture + '-' + ddFuture;
+function search(ele) {
+	if (event.key === 'Enter') {
+		getInput();
+	}
 }
 
-computedDates.push(backDates.reverse());
-computedDates.push(today);
-computedDates.push(futureDates);
-var compliedDates = computedDates.flatMap((x) => x);
+function getValues(){
 
-console.log(compliedDates)
+	function dateDifference(from, to) {
+		from = new Date(from);
+		to = new Date(to);
+	
+		const utc1 = Date.UTC(
+			from.getFullYear(),
+			from.getMonth(),
+			from.getDate(),
+		);
+		const utc2 = Date.UTC(
+			to.getFullYear(),
+			to.getMonth(),
+			to.getDate(),
+		);
+		const msPerDay = 1000 * 60 * 60 * 24;
+	
+		return Math.floor((utc2 - utc1) / msPerDay);
+	}
+	
+	for (i = 1; i <= 30; i++) {
+		//past seven days
+		var back = new Date(today);
+		back.setDate(back.getDate() - i);
+		var ddBack = back.getDate();
+		var mmBack = back.getMonth() + 1;
+		var yyBack = back.getFullYear();
+		//backDates[i] = yyBack + '-' + mmBack + '-' + ddBack;
+	
+		//future seven days
+		var future = new Date(today);
+		future.setDate(future.getDate() + i);
+		var ddFuture = future.getDate();
+		var mmFuture = future.getMonth() + 1;
+		var yyFuture = future.getFullYear();
+		futureDates[i] =
+			yyFuture + '-' + mmFuture + '-' + ddFuture;
+	}
 
+	computedDates.push(backDates.reverse());
+	computedDates.push(today);
+	computedDates.push(futureDates);
+	var compliedDates = computedDates.flatMap((x) => x);
+	
+	for (i = 0; i <= 30; i++) {
+		differences[i] = dateDifference(fetchedDate,compliedDates[i]);
+		intellectualValues[i] = Math.round(
+			100 *
+				Math.sin((2 * pi * differences[i]) / intellectualCycle),
+		);
+		physicalValues[i] = Math.round(
+			100 * Math.sin((2 * pi * differences[i]) / physicalCycle),
+		);
+		emotionalValues[i] = Math.round(
+			100 *
+				Math.sin((2 * pi * differences[i]) / emotionalCycle),
+		);
+	}
+	
+	console.log(intellectualValues[0]);
+	console.log(physicalValues[0]);
+	console.log(emotionalValues[0]);
 
-for (i = 0; i <= 30; i++) {
-	differences[i] = dateDifference('1996-8-19', compliedDates[i]);
-	intellectualValues[i] =Math.round(100 * Math.sin((2 * pi * differences[i]) / intellectualCycle));
-	physicalValues[i] =Math.round(100 * Math.sin((2 * pi * differences[i]) / physicalCycle));
-	emotionalValues[i] =Math.round(100 * Math.sin((2 * pi * differences[i]) / emotionalCycle));
 }
-
-console.log(intellectualValues[0])
-console.log(physicalValues[0])
-console.log(emotionalValues[0])
--- style.js
+++ style.js
@@ -7,6 +7,7 @@ setTimeout(popLogoReveal, 0000);
 
 var date = document.getElementById('dob');
 
+
 function checkValue(str, max) {
 	if (str.charAt(0) !== '0' || str == '00') {
 		var num = parseInt(str);
@@ -28,128 +29,122 @@ date.addEventListener('input', function (e) {
 	var values = input.split('/').map(function (v) {
 		return v.replace(/\D/g, '');
 	});
+	if (values[0]) values[0] = checkValue(values[0], 31);
 	if (values[1]) values[1] = checkValue(values[1], 12);
-	if (values[2]) values[2] = checkValue(values[2], 31);
 	var output = values.map(function (v, i) {
-		return v.length == 4 && i < 2 ? v + ' / ' : v;
+		return v.length == 2 && i < 2 ? v + ' / ' : v;
 	});
 	this.value = output.join('').substr(0, 14);
 });
 
 //d3
 
+function loadGraph(){
 
-const dataBinder1 = intellectualValues.map((x, i) => {
-	return [i * 10, x];
-});
-
-const dataBinder2 = physicalValues.map((x, i) => {
-	return [i * 10, x];
-});
-
-const dataBinder3 = emotionalValues.map((x, i) => {
-	return [i * 10, x];
-});
-
-var coordsIntellectual = dataBinder1;
-var coordsPhysical = dataBinder2;
-var coordsEmotional = dataBinder3;
-
-var scale = d3
-	.scaleLinear()
-	.domain([-120, 120])
-	.range([200, 0]);
-
-var bezierLine = d3
-	.line()
-	.x(function (d) {
-		return d[0];
-	})
-	.y(function (d) {
-		return scale(d[1]);
-	})
-	.curve(d3.curveBasis);
-
-var svg1 = d3
-	.select('#intellectual')
-	.append('svg')
-	.attr('width', 400)
-	.attr('height', 240)
-	//.attr("viewBox", "0 0 300 150") 300 150
-	.attr('transform', 'scale(0.5)')
-	.attr('preserveAspectRatio', 'xMidYMid meet');
-
-var svg2 = d3
-	.select('#physical')
-	.append('svg')
-	.attr('width', 400)
-	.attr('height', 240)
-	.attr('transform', 'scale(0.5)')
-	.attr('preserveAspectRatio', 'xMidYMid meet');	
-
-var svg3 = d3
-	.select('#emotional')
-	.append('svg')
-	.attr('width', 400)
-	.attr('height', 240)
-	.attr('transform', 'scale(0.5)')
-	.attr('preserveAspectRatio', 'xMidYMid meet');	
-
-const path1 = svg1
-	.append('path')
-	.attr('d', bezierLine(coordsIntellectual))
-	.attr('stroke', 'white')
-	.attr('stroke-width', 8)
-	.attr('stroke-linecap', 'round')
-	.attr('fill', 'none')
-	.transition()
-	.duration(1500)
-	.attrTween('stroke-dasharray', function () {
-		var len = this.getTotalLength();
-		return function (t) {
-			return d3.interpolateString('0,' + len, len + ',0')(t);
-		};
+	const dataBinder1 = intellectualValues.map((x, i) => {
+		return [i * 20, x];
 	});
-
-const path2 = svg2
-	.append('path')
-	.attr('d', bezierLine(coordsPhysical))
-	.attr('stroke', 'red')
-	.attr('stroke-width', 8)
-	.attr('stroke-linecap', 'round')
-	.attr('fill', 'none')
-	.transition()
-	.duration(1500)
-	.attrTween('stroke-dasharray', function () {
-		var len = this.getTotalLength();
-		return function (t) {
-			return d3.interpolateString('0,' + len, len + ',0')(t);
-		};
+	
+	const dataBinder2 = physicalValues.map((x, i) => {
+		return [i * 20, x];
 	});
-
-const path3 = svg3
-	.append('path')
-	.attr('d', bezierLine(coordsEmotional))
-	.attr('stroke', 'blue')
-	.attr('stroke-width', 8)
-	.attr('stroke-linecap', 'round')
-	.attr('fill', 'none')
-	.transition()
-	.duration(1500)
-	.attrTween('stroke-dasharray', function () {
-		var len = this.getTotalLength();
-		return function (t) {
-			return d3.interpolateString('0,' + len, len + ',0')(t);
-		};
+	
+	const dataBinder3 = emotionalValues.map((x, i) => {
+		return [i * 20, x];
 	});
+	
+	var coordsIntellectual = dataBinder1;
+	var coordsPhysical = dataBinder2;
+	var coordsEmotional = dataBinder3;
+	
+	var scale = d3
+		.scaleLinear()
+		.domain([-240, 240])
+		.range([300, 0]);
+	
+	var bezierLine = d3
+		.line()
+		.x(function (d) {
+			return d[0];
+		})
+		.y(function (d) {
+			return scale(d[1]);
+		})
+		.curve(d3.curveBasis);
+	
+	var svg1 = d3
+		.select('#intellectual')
+		.append('svg')
+		.attr('width', 800)
+		.attr('height', 240)
+		//.attr("viewBox", "0 0 800 240")
+		.attr("preserveAspectRatio", "xMinYMin meet")
+		.classed("svg-content", true)
+		//.attr('transform', 'scale(0.5)')
+	
+	var svg2 = d3
+		.select('#physical')
+		.append('svg')
+		.attr('width', 800)
+		.attr('height', 240)
+		//.attr('transform', 'scale(0.5)')
+	
+	var svg3 = d3
+		.select('#emotional')
+		.append('svg')
+		.attr('width', 800)
+		.attr('height', 240)
+		//.attr('transform', 'scale(0.5)')
+	
+	const path1 = svg1
+		.append('path')
+		.attr('d', bezierLine(coordsIntellectual))
+		.attr('stroke', '#72DEC2')
+		.attr('stroke-width', 8)
+		.attr('stroke-linecap', 'round')
+		.attr('fill', 'none')
+		.transition()
+		.duration(1500)
+		.attrTween('stroke-dasharray', function () {
+			var len = this.getTotalLength();
+			return function (t) {
+				return d3.interpolateString('0,' + len, len + ',0')(t);
+			};
+		});
+	
+	const path2 = svg2
+		.append('path')
+		.attr('d', bezierLine(coordsPhysical))
+		.attr('stroke', '#FFB545')
+		.attr('stroke-width', 8)
+		.attr('stroke-linecap', 'round')
+		.attr('fill', 'none')
+		.transition()
+		.duration(1500)
+		.attrTween('stroke-dasharray', function () {
+			var len = this.getTotalLength();
+			return function (t) {
+				return d3.interpolateString('0,' + len, len + ',0')(t);
+			};
+		});
+	
+	const path3 = svg3
+		.append('path')
+		.attr('d', bezierLine(coordsEmotional))
+		.attr('stroke', 'white')
+		.attr('stroke-width', 8)
+		.attr('stroke-linecap', 'round')
+		.attr('fill', 'none')
+		.transition()
+		.duration(1500)
+		.attrTween('stroke-dasharray', function () {
+			var len = this.getTotalLength();
+			return function (t) {
+				return d3.interpolateString('0,' + len, len + ',0')(t);
+			};
+		});
 
-
-
-
-
-
-
-
+}
 
 
 let xs = [];
