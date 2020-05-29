63e33d88ec47355a17118bdfbe229deafb115c33
rtikha <raunakstrikha@gmail.com>
2020-05-29 18:58:02 +0530

    adding styles for postBrowserCheck()

--- style.js
+++ style.js
@@ -1,5 +1,5 @@
 var supportablewWidth = 560;
-var logoReveal = 1000;
+var logoReveal = 4000;
 
 animationGetter('prime-holder', 'display', 'none');
 animationGetter('response', 'display', 'none');
@@ -59,95 +59,99 @@ if (screen.width <= supportablewWidth) {
 		(function (p) {
 			return p.toString() === '[object SafariRemoteNotification]';
 		})(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-	var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
+	var isIE = /*@cc_on!@*/ false || !!document.documentMode;
 
 	if (isFirefox == true) {
 		elementRemover('line-chart');
 		document.getElementById('chart-title-month').innerHTML = 'We dont support month view in this browser. For best experience use Chrome';
 		sampleWave();
-	} else if (isSafari == true || isIE==true) {
+		postBrowserCheck();
+	} else if (isSafari == true || isIE == true) {
 		elementRemover('response');
 		elementRemover('dob');
 		sampleWave();
 		document.getElementById('dob-title').innerHTML = 'We dont support this browser as of now. For best experience use Chrome';
 	} else {
-		setTimeout(function () {
-			animationGetter('footer', 'display', 'block');
-			animationGetter('footer', 'opacity', '1');
-		}, logoReveal + 1400);
-
-		/*
-	
-	*	DOB input field modulator
-	
-	*/
-
-		var date = document.getElementById('dob');
-
-		function checkValue(str, max) {
-			if (str.charAt(0) !== '0' || str == '00') {
-				var num = parseInt(str);
-				if (isNaN(num) || num <= 0 || num > max) num = 1;
-				str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();
-			}
-			return str;
-		}
-
-		date.addEventListener('input', function (e) {
-			this.type = 'text';
-			var input = this.value;
-			if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
-			var values = input.split('/').map(function (v) {
-				return v.replace(/\D/g, '');
-			});
-			if (values[0]) values[0] = checkValue(values[0], 31);
-			if (values[1]) values[1] = checkValue(values[1], 12);
-			var output = values.map(function (v, i) {
-				return v.length == 2 && i < 2 ? v + ' / ' : v;
-			});
-			this.value = output.join('').substr(0, 14);
-		});
-
-		/*
-	
-	*	Sample wave animation for first-hit
-	
-	*/
-
-		sampleWave();
-
-		/*
-	
-	*	Modal for about
-	
-	*/
-
-		var modal = document.querySelector('.modal');
-		var trigger = document.querySelector('.about');
-		var closeButton = document.querySelector('.close-button');
-
-		function toggleModal() {
-			modal.classList.toggle('show-modal');
-		}
-
-		function windowOnClick(event) {
-			if (event.target === modal) {
-				toggleModal();
-			}
-		}
-
-		trigger.addEventListener('click', toggleModal);
-		closeButton.addEventListener('click', toggleModal);
-		window.addEventListener('click', windowOnClick);
-
-		/*
-	
-	*	Chart headings
-	
-	*/
-
-		var dateToday = Date.parse(Date.today()).toString('dddd dd MMMM');
-		document.getElementById('chart-title-today').innerHTML = `Biorhythm for Today, ${dateToday}`;
+		postBrowserCheck();
 	}
+}
+
+function postBrowserCheck() {
+	setTimeout(function () {
+		animationGetter('footer', 'display', 'block');
+		animationGetter('footer', 'opacity', '1');
+	}, logoReveal + 1400);
+
+	/*
+
+*	DOB input field modulator
+
+*/
+
+	var date = document.getElementById('dob');
+
+	function checkValue(str, max) {
+		if (str.charAt(0) !== '0' || str == '00') {
+			var num = parseInt(str);
+			if (isNaN(num) || num <= 0 || num > max) num = 1;
+			str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();
+		}
+		return str;
+	}
+
+	date.addEventListener('input', function (e) {
+		this.type = 'text';
+		var input = this.value;
+		if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
+		var values = input.split('/').map(function (v) {
+			return v.replace(/\D/g, '');
+		});
+		if (values[0]) values[0] = checkValue(values[0], 31);
+		if (values[1]) values[1] = checkValue(values[1], 12);
+		var output = values.map(function (v, i) {
+			return v.length == 2 && i < 2 ? v + ' / ' : v;
+		});
+		this.value = output.join('').substr(0, 14);
+	});
+
+	/*
+
+*	Sample wave animation for first-hit
+
+*/
+
+	sampleWave();
+
+	/*
+
+*	Modal for about
+
+*/
+
+	var modal = document.querySelector('.modal');
+	var trigger = document.querySelector('.about');
+	var closeButton = document.querySelector('.close-button');
+
+	function toggleModal() {
+		modal.classList.toggle('show-modal');
+	}
+
+	function windowOnClick(event) {
+		if (event.target === modal) {
+			toggleModal();
+		}
+	}
+
+	trigger.addEventListener('click', toggleModal);
+	closeButton.addEventListener('click', toggleModal);
+	window.addEventListener('click', windowOnClick);
+
+	/*
+
+*	Chart headings
+
+*/
+
+	var dateToday = Date.parse(Date.today()).toString('dddd dd MMMM');
+	document.getElementById('chart-title-today').innerHTML = `Biorhythm for Today, ${dateToday}`;
 }
